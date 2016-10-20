20e245765668385f0604da0764b7d4c71334a7f2
Richard Jiang <richard.jiang@delawareconsulting.com>
2016-10-20 21:19:01 +0800

    init

---
+++ testDemo/index.ios.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import configureStore from './js/store/configureStore'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import App from './js/containers/App'
+
+class testDemo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      store: configureStore()
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={this.state.store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+AppRegistry.registerComponent('testDemo', () => testDemo);
